1926d1897b622320e40cbb9ef97fda3c4eca4588
Rinat <51479797+Xrenya@users.noreply.github.com>
2021-07-12 15:59:50 +0900

    Update credit.c

--- pset_1/credit/credit.c
+++ pset_1/credit/credit.c
@@ -7,12 +7,14 @@ int main(void)
 
     int odd_summator = 0;
     int even_summator = 0;
-
+    int last_digit = 0;
     int position = 1;
-
+    int two_digit = 0;
+    int previous_digit = 0;
     while (num > 0)
     {
-        int last_digit = num % 10;
+        previous_digit = last_digit;
+        last_digit = num % 10;
         num = num / 10;
 
         if (position % 2 == 0)
@@ -28,9 +30,26 @@ int main(void)
 
         position++;
     }
+    position -= 1;
+    two_digit = last_digit * 10 + previous_digit % 10;
     if ((even_summator + odd_summator) % 10 == 0)
     {
-        printf("VISA\n");
+        if (position >= 13 && position <= 16 && last_digit == 4)
+        {
+            printf("VISA\n");
+        }
+        else if (position == 16 && two_digit >= 51 && two_digit <= 55)
+        {
+            printf("MASTERCARD\n");
+        }
+        else if (position == 15 && (two_digit == 34 || two_digit == 37))
+        {
+            printf("AMEX\n");
+        }
+        else
+        {
+            printf("INVALID\n");
+        }
     }
     else
     {
